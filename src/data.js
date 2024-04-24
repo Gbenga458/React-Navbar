add13ed29b8530c819d6355784f82f806b8c45ca
unknown <gbengaTec>
2024-04-24 15:31:20 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {FaBehance, FaFacebook, FaTwitter, FaLinkedin} from 'react-icons/fa';
+
+export const links =[
+  {
+    id: 1,
+    url:'/',
+    text:'home'
+  },
+   {
+    id: 2,
+    url:'/About',
+    text:'About'
+  },
+   {
+    id: 3,
+    url:'/Projects',
+    text:'Projects'
+  },
+   {
+    id: 4,
+    url:'/Contact',
+    text:'Contact'
+  },
+   {
+    id: 5,
+    url:'/Profile',
+    text:'Profile'
+  },
+]
+
+export const social =[
+  {
+    id: 1,
+    url: "www.https://twitter.com",
+    icon:<FaTwitter/>
+  },
+  {
+    id: 1,
+    url: "www.https://facebook.com",
+    icon:<FaFacebook/>
+  },
+  {
+    id: 1,
+    url: "www.https://linkedin.com",
+    icon:<FaLinkedin/>
+  },
+  {
+    id: 1,
+    url: "www.https://behance.com",
+    icon:<FaBehance/>
+  }
+]
